c683863ae41220116a505b9d76ee6da4c9d6f7c3
nileshjd2202 <nileshdangi2016@gmail.com>
2023-02-11 13:43:36 +0530

    class_based_apiview

--- university_project_2/api/views.py
+++ university_project_2/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Student
 from .serializers import StudentSerializer
+from rest_framework import status
 
 
 # Create your views here.
